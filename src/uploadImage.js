ca166e142c78eb4196212bb362310da68c31d148
Crzzphillip <87367342+Crzzphillip@users.noreply.github.com>
2025-07-03 13:03:40 +0200

    Add uploadImage.js function template

---
+++ src/uploadImage.js
@@ -0,0 +1,8 @@
+// Upload/Update Image Function
+// Receives: { id, image }
+// Returns: Updated workflow document or error
+
+module.exports = async function (req, res) {
+  // TODO: Implement Appwrite SDK logic to upload/update image
+  res.json({ message: 'Upload/Update Image function template' });
+};
