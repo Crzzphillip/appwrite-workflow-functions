e762cd63353cc0e5fdf463df332f0fdfa8956d97
Crzzphillip <87367342+Crzzphillip@users.noreply.github.com>
2025-07-03 13:03:28 +0200

    Add updateWorkflow.js function template

---
+++ src/updateWorkflow.js
@@ -0,0 +1,8 @@
+// Update Workflow Function
+// Receives: { id, updates }
+// Returns: Updated workflow document or error
+
+module.exports = async function (req, res) {
+  // TODO: Implement Appwrite SDK logic to update a workflow
+  res.json({ message: 'Update Workflow function template' });
+};
