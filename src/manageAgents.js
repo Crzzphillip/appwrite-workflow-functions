92e66668f43732c770a1e99fdc26628703df33c5
Crzzphillip <87367342+Crzzphillip@users.noreply.github.com>
2025-07-03 13:03:37 +0200

    Add manageAgents.js function template

---
+++ src/manageAgents.js
@@ -0,0 +1,8 @@
+// Manage Agents Function
+// Receives: { id, agents }
+// Returns: Updated workflow document or error
+
+module.exports = async function (req, res) {
+  // TODO: Implement Appwrite SDK logic to add/remove agents
+  res.json({ message: 'Manage Agents function template' });
+};
