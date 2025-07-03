443788b5d679dbc48d45134a1da350c9d2341c21
Crzzphillip <87367342+Crzzphillip@users.noreply.github.com>
2025-07-03 13:03:27 +0200

    Add listWorkflows.js function template

---
+++ src/listWorkflows.js
@@ -0,0 +1,8 @@
+// List Workflows Function
+// Receives: { filters }
+// Returns: Array of workflow documents or error
+
+module.exports = async function (req, res) {
+  // TODO: Implement Appwrite SDK logic to list workflows
+  res.json({ message: 'List Workflows function template' });
+};
