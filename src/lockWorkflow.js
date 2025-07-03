e28564470af3b1aedf42a8449b9132126caceac4
Crzzphillip <87367342+Crzzphillip@users.noreply.github.com>
2025-07-03 13:03:36 +0200

    Add lockWorkflow.js function template

---
+++ src/lockWorkflow.js
@@ -0,0 +1,8 @@
+// Lock/Unlock Workflow Function
+// Receives: { id, locked }
+// Returns: Updated workflow document or error
+
+module.exports = async function (req, res) {
+  // TODO: Implement Appwrite SDK logic to lock/unlock a workflow
+  res.json({ message: 'Lock/Unlock Workflow function template' });
+};
