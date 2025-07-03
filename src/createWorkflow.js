32c4938704dc8152bd8ccf94065d9417132909c9
Crzzphillip <87367342+Crzzphillip@users.noreply.github.com>
2025-07-03 13:03:25 +0200

    Add createWorkflow.js function template

---
+++ src/createWorkflow.js
@@ -0,0 +1,8 @@
+// Create Workflow Function
+// Receives: { title, description, credits, locked, image, agents, author_id, tags, n8n_json }
+// Returns: Created workflow document or error
+
+module.exports = async function (req, res) {
+  // TODO: Implement Appwrite SDK logic to create a workflow
+  res.json({ message: 'Create Workflow function template' });
+};
