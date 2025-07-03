38c8167784bfbf3813539d22a6442912cea6596c
Crzzphillip <87367342+Crzzphillip@users.noreply.github.com>
2025-07-03 13:03:39 +0200

    Add searchWorkflows.js function template

---
+++ src/searchWorkflows.js
@@ -0,0 +1,8 @@
+// Search Workflows Function
+// Receives: { query }
+// Returns: Array of workflow documents or error
+
+module.exports = async function (req, res) {
+  // TODO: Implement Appwrite SDK logic to search workflows
+  res.json({ message: 'Search Workflows function template' });
+};
