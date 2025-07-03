4756a8f7b72c8b02415ff1a3ac93ad9135fe1f60
Crzzphillip <87367342+Crzzphillip@users.noreply.github.com>
2025-07-03 13:03:38 +0200

    Add updateUsageStats.js function template

---
+++ src/updateUsageStats.js
@@ -0,0 +1,8 @@
+// Update Usage Stats Function
+// Receives: { id, usageStats }
+// Returns: Updated workflow document or error
+
+module.exports = async function (req, res) {
+  // TODO: Implement Appwrite SDK logic to update usage stats
+  res.json({ message: 'Update Usage Stats function template' });
+};
