5ab07f74863c9d912b15e9084565f9751cf12f70
Crzzphillip <87367342+Crzzphillip@users.noreply.github.com>
2025-07-03 13:10:27 +0200

    Add constants.js for DB and collection IDs.

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+// Constants for database and collection IDs
+module.exports = {
+  DATABASE_ID: process.env.APPWRITE_DATABASE_ID || '680f8877003e0a037759',
+  COLLECTION_ID: process.env.APPWRITE_COLLECTION_ID || 'workflows',
+};
