184c3e1f0690447af52c8e370c9f0cc88d1c88d1
Crzzphillip <87367342+Crzzphillip@users.noreply.github.com>
2025-07-03 13:03:26 +0200

    Add getWorkflow.js function template

---
+++ src/getWorkflow.js
@@ -0,0 +1,8 @@
+// Get Workflow Function
+// Receives: { id }
+// Returns: Workflow document or error
+
+module.exports = async function (req, res) {
+  // TODO: Implement Appwrite SDK logic to get a workflow by ID
+  res.json({ message: 'Get Workflow function template' });
+};
