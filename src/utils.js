457f2ddd26a9cb6ae174a63659941b2ce5816899
Crzzphillip <87367342+Crzzphillip@users.noreply.github.com>
2025-07-03 13:10:25 +0200

    Add utils.js for Appwrite client setup.

---
+++ src/utils.js
@@ -0,0 +1,11 @@
+// Utility for Appwrite client setup
+const sdk = require('appwrite');
+
+const client = new sdk.Client()
+  .setEndpoint(process.env.APPWRITE_ENDPOINT)
+  .setProject(process.env.APPWRITE_PROJECT_ID)
+  .setKey(process.env.APPWRITE_API_KEY);
+
+const databases = new sdk.Databases(client);
+
+module.exports = { client, databases, sdk };
