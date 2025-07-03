7144e1c121e4b1c9578b5bcfd489ae8f3b792d5d
Crzzphillip <87367342+Crzzphillip@users.noreply.github.com>
2025-07-03 13:03:29 +0200

    Add deleteWorkflow.js function template

---
+++ src/deleteWorkflow.js
@@ -0,0 +1,8 @@
+// Delete Workflow Function
+// Receives: { id }
+// Returns: Success or error
+
+module.exports = async function (req, res) {
+  // TODO: Implement Appwrite SDK logic to delete a workflow
+  res.json({ message: 'Delete Workflow function template' });
+};
