a57641cbf6cefbe6737ad4e8cd47e6b426e818e4
Crzzphillip <87367342+Crzzphillip@users.noreply.github.com>
2025-07-03 13:03:41 +0200

    Add updateN8nJson.js function template

---
+++ src/updateN8nJson.js
@@ -0,0 +1,8 @@
+// Update n8n_json Function
+// Receives: { id, n8n_json }
+// Returns: Updated workflow document or error
+
+module.exports = async function (req, res) {
+  // TODO: Implement Appwrite SDK logic to update n8n_json
+  res.json({ message: 'Update n8n_json function template' });
+};
