c0629e56334b2f95c9dc2d5e584607f6bd9af776
Crzzphillip <87367342+Crzzphillip@users.noreply.github.com>
2025-07-04 10:43:16 +0200

    Update listWorkflows.js

--- src/listWorkflows.js
+++ src/listWorkflows.js
@@ -1,11 +1,9 @@
-const { databases } = require('./utils');
-const { DATABASE_ID, COLLECTION_ID } = require('./constants');
+// const { databases } = require('./utils');
+// const { DATABASE_ID, COLLECTION_ID } = require('./constants');
 
 export default async ({ req, res }) => {
   
   try {
-   
-   
   return res.json({message: 'done exercuting...'},200);
   } catch (err) {
   return res.json({ error: err.message },500);
